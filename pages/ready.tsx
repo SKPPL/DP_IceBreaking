ecaf43f437cc79aaedb8602a950a9c7bab2ad0ab
hyunsik <hyunsig96@gmail.com>
2023-02-24 20:51:14 +0900

    1st: ready page  refactoring

--- pages/ready.tsx
+++ pages/ready.tsx
@@ -1,9 +1,5 @@
-import { createContext } from "react";
 import Head from "next/head";
 import RoomList from "@/components/WebRTC/RoomList";
-import styles from "../components/PageElements/styles.module.css";
-import Tutorial from "@/components/PageElements/Modal/Tutorial";
-const MyContext = createContext("");
 
 export default function Ready() {
   return (
@@ -11,10 +7,7 @@ export default function Ready() {
       <Head>
         <title>Ready</title>
       </Head>
-      <div className={styles.readyBox}>
         <RoomList />
-      </div>
-      <Tutorial />
     </>
   );
 }
