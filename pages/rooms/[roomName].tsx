73e89680a219f436f07d71ce7913c5644f2f6ffc
hyunsik <hyunsig96@gmail.com>
2023-02-26 20:03:25 +0900

    fix: progress bar 애니메이션효과 추가 및 tensorflow 버전 재수정

--- pages/rooms/[roomName].tsx
+++ pages/rooms/[roomName].tsx
@@ -33,8 +33,8 @@ export default function Play() {
             <RecoilRoot>
                 <Provider store={store}>
                     <WebRTC />
-                    <MakeFaceLandMark i={'host'} />
-                    <MakeFaceLandMark i={'peer'} />
+                    {/* <MakeFaceLandMark i={'host'} /> */}
+                    {/* <MakeFaceLandMark i={'peer'} /> */}
                     <ItemBar />
                 </Provider>
             </RecoilRoot>
