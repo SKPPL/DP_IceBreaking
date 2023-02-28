4c546eca53eae6cb2f8766ab33347a7d4cf40bea
jeonje <whssodi@gmail.com>
2023-02-28 16:58:10 +0900

    fixed : item tutorial description change video version

--- components/PageElements/Modal/Tutorial.tsx
+++ components/PageElements/Modal/Tutorial.tsx
@@ -10,26 +10,30 @@ const SKILLS = [
   {
     name: "히히 로켓발싸!",
     image: "/images/rocket.png",
+    GIF: "/images/rocket_gif.gif",
+    VIDEO: "/images/rocket_video.mp4",
     description: "9초동안 상대방 퍼즐이 귀여운 강아지가 탄 로켓으로 변합니다. 상대방이 퍼즐을 맞추지 못하도록 우주 멀리 날려보내세요.",
-    videoUrl: "https://www.youtube.com/embed/60RaggLgggE",
   },
   {
     name: "확 다 얼려부려ㅎ",
     image: "/images/iceIcon.png",
+    GIF: "/images/ice_gif.gif",
+    VIDEO: "/images/ice_video.mp4",
     description: "15초동안 상대방 퍼즐이 움직이지 못하도록 얼려버립니다. 잠깐! 얼어붙은 카드는 상대방이 열심히 클릭하여 녹일 수 있습니다.",
-    videoUrl: "https://www.youtube.com/embed/VkCRg_p9-_A",
   },
   {
     name: "ㅋㅋ블랙홀이요~",
     image: "/images/magnet.png",
-    description: "응 자석쓰~ 7초동안 상대방 마우스 포인터가 블랙홀로 변합니다. 상대방이 기껏 정성스레 맞춰놓은 퍼즐까지 모두 다 빨아드립니다..",
-    videoUrl: "https://www.youtube.com/embed/8GERsPyC4_A",
+    GIF: "/images/magnet_gif.gif",
+    VIDEO: "/images/magnet_video.mp4",
+    description: "응 블랙홀~ 7초동안 상대방 마우스 포인터가 블랙홀로 변합니다. 상대방이 기껏 정성스레 맞춰놓은 퍼즐까지 모두 다 빨아드립니다..",
   },
   {
     name: "입술 쪽",
     image: "/images/lip.png",
+    GIF: "/images/lip_gif.gif",
+    VIDEO: "/images/lip_video.mp4",
     description: "10초동안 상대방 퍼즐이 내 입술로 변합니다. 매혹적인 입술로 상대방을 당황시키세요~ >3<",
-    videoUrl: "https://www.youtube.com/embed/8GERsPyC4_A",
   },
 ];
 
@@ -59,7 +63,7 @@ export default function Tutorial() {
         ?
       </button>
       <Modal size="xl" aria-labelledby="contained-modal-title-vcente" centered show={isOpen} onHide={closeModal}>
-        <Modal.Header closeButton className="bg-black">
+        <Modal.Header className="bg-black" closeButton>
           <Modal.Title>
             <h1 className="text-white">스킬</h1>
 
@@ -90,24 +94,47 @@ export default function Tutorial() {
         </Modal.Header>
         <Modal.Body className="bg-black">
           <section className="bg-black dark:bg-gray-900">
-            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-              <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <div className="gap-16 items-center py-2 px-4 mx-auto max-w-screen-lg lg:grid lg:grid-cols-3 lg:py-16 lg:px-6">
+              <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400 col-span-1">
                 <Image src={currentSkill.image} alt="" width={60} height={60} />
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white ">{currentSkill.name}</h2>
                 <p className="mb-4 text-2xl text-slate-400">{currentSkill.description}</p>
               </div>
-              <div className="grid grid-cols-2 gap-4 mt-8">
-                <iframe
-                  title="Video"
-                  width="300"
-                  height="300"
-                  src={`${currentSkill.videoUrl}?autoplay=1&mute=1`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                />
+              <div className="grid col-span-2">
+                {/* <div style={{ position: "relative", width: "100%", height: "0", paddingBottom: "66.67%" }}> */}
+                {/* <Image src={currentSkill.GIF} alt={currentSkill.name} fill style={{ objectFit: "fill" }} /> */}
+                <div
+                  style={{
+                    position: "relative",
+                    height: 0,
+                    paddingBottom: "56.25%",
+                  }}
+                >
+                  <video
+                    src={currentSkill.VIDEO}
+                    autoPlay
+                    muted
+                    loop
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "fill",
+                    }}
+                  />
+                  {/* </div> */}
+                </div>
               </div>
             </div>
           </section>
         </Modal.Body>
+        <Modal.Footer className="bg-black">
+          <Button variant="danger" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   );
