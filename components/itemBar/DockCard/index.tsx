ab6bd4c4280dd4ede57828ed44f207e7348da886
hyunsik <hyunsig96@gmail.com>
2023-03-04 23:33:53 +0900

    fix: 아이템바 안써지는거 수정 및 색수정

--- components/itemBar/DockCard/index.tsx
+++ components/itemBar/DockCard/index.tsx
@@ -10,7 +10,7 @@ import { useDock } from '../Dock/DockContext';
 
 import styles from './styles.module.scss';
 import { useRecoilValue } from 'recoil';
-import { dataChannelState, peerLipState, peerTwirlState, peerWaitState } from "../../Game/atom";
+import { peerLipState, peerTwirlState, peerWaitState } from "../../Game/atom";
 
 interface DockCardProps {
   children: React.ReactNode;
@@ -86,14 +86,13 @@ export const DockCard = ({ children, item }: DockCardProps) => {
   const wasUsed = React.useRef(false);
 
   const peerWait = useRecoilValue(peerWaitState);
-  const dataChannel = useRecoilValue(dataChannelState);
   const peerLipWait = useRecoilValue(peerLipState);
   const peerTwirlWait = useRecoilValue(peerTwirlState);
   const itemWait = peerLipWait || peerTwirlWait;
 
   const handleClick = () => {
     // peer에 쓴 아이템이 진행중일 때는 눌러도 아무일도 일어나지 않음
-    if (peerWait === 9 || itemWait === true || dataChannel === false) return;
+    if (peerWait === 9 || itemWait === true) return;
     dispatch({ type: `item/${itemList[item]}` });
     if (!wasUsed.current) {
       wasUsed.current = true;
