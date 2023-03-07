57235f8f8c6dc21ceb158416156e4380578b5ac9
h0y301v <h0y301v@gmail.com>
2023-03-07 11:16:49 +0900

    fix: sound volume up (compression,makeup)

--- components/PageElements/Loading.tsx
+++ components/PageElements/Loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className={styles.container}>
       <div className={styles.text}>
-        다이나믹 퍼즐이 준비 중입니다.
+        다이나믹 퍼즐을 준비 중입니다.
       </div>
     </div>
   );
