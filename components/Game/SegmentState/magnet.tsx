d04ff353e115d3c50be986b6e96b0a54fe02842b
hyunsik <hyunsig96@gmail.com>
2023-03-09 19:02:15 +0900

    fix: magnet 불필요 움직임 데이터전송 삭제

--- components/Game/SegmentState/magnet.tsx
+++ components/Game/SegmentState/magnet.tsx
@@ -57,7 +57,7 @@ export default function magnet({ i, auth, videoId, peerxy, dataChannel, segmentS
             api.start({ xy: [mpx, mpy], delay: 0 });
             //나의 움직임을 상대방에게 그리기 위해 정보전달
             if (dataChannel) {
-                dataChannel.send(JSON.stringify({ type: "move", i: i, peerx: mpx, peery: mpy }));
+                // dataChannel.send(JSON.stringify({ type: "move", i: i, peerx: mpx, peery: mpy }));
                 dataChannel.send(
                     JSON.stringify({
                         type: "magnet",
